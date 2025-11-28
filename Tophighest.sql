add75a3a439436c471d13cc6580bdccf94405fcc
kebalkhadka <kebalkhadka7@gmail.com>
2025-11-28 17:37:28 +0545

    new solution solved

--- Tophighest.sql
+++ Tophighest.sql
@@ -14,3 +14,15 @@ group by p.patient_id, p.full_name
 order by total_sales desc
 limit 3;
 
+-- List all patients along with the medicines prescribed to them in the last month.
+
+select 
+p.patient_id,p.full_name,pr.appointment_id,a.appointment_date,
+pr.medicine_name
+from 
+patients p 
+join 
+appointments a on p.patient_id = a.patient_id
+left join 
+prescriptions pr on pr.appointment_id = a.appointment_id
+where a.appointment_date >= date_sub(curdate(),interval 6 month);
